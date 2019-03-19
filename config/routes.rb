bf75aa90273f2de5f7de15bcda23bbd908b36cb0
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-03-19 16:31:14 +0900

    投稿編集削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
   get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
