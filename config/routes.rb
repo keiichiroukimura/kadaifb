73bbbccf251c500f62e6e54dcb998a010588a09b
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-03-27 11:05:48 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  get 'sessions/new'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users,only:[:new, :create, :show]
