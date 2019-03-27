1380e84584c77734c8d51ba411ff6cb00c64cec0
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-03-27 11:10:12 +0900

    herokuup

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users,only:[:new, :create, :show]
   resources :sessions
 end
